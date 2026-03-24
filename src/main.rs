009042a33ad1ca0a2f711132998204d6833ac435
void4main <void@mailbox.org>
2026-03-24 13:10:38 +0100

    Feature: nop in loop

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,33 @@
+use std::arch::asm;
+use kuva::plot::SankeyPlot;
+use kuva::backend::svg::SvgBackend;
+use kuva::render::render::render_multiple;
+use kuva::render::layout::Layout;
+use kuva::render::plots::Plot;
+
+
 fn main() -> ! {
     println!("Hello, world!");
+
+
+    let sankey = SankeyPlot::new()
+        .with_link("Input", "Process A", 50.0)
+        .with_link("Input", "Process B", 30.0)
+        .with_link("Process A", "Output X", 40.0)
+        .with_link("Process A", "Output Y", 10.0)
+        .with_link("Process B", "Output X", 10.0)
+        .with_link("Process B", "Output Y", 20.0);
+
+    let plots = vec![Plot::Sankey(sankey)];
+    let layout = Layout::auto_from_plots(&plots)
+        .with_title("Energy Flow");
+
+    let svg = SvgBackend.render_scene(&render_multiple(plots, layout));
+    std::fs::write("sankey.svg", svg).unwrap();
+    println!("Done!");
     loop {
-        todo!("nop");
+        unsafe {
+            asm!("nop");
+        }
     }
 }
